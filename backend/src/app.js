ba6d72d530e0801b6ac7f772b1db23b3053ef0a4
Milan Bhimani <milanhbhimani>
2025-01-07 15:58:10 +0530

    changed database to mongodb atlas

--- backend/src/app.js
+++ backend/src/app.js
@@ -24,6 +24,6 @@ app.use('/api', itemRoutes);
 app.use('/auth', authRoutes);
 app.use('/user', userRoutes); // Use user routes
 
-mongoose.connect(process.env.MONGO_URI, { useNewUrlParser: true, useUnifiedTopology: true })
+mongoose.connect(process.env.MONGO_URI)
   .then(() => app.listen(PORT, () => console.log(`Server running on port ${PORT}`)))
   .catch((error) => console.log(error.message));
