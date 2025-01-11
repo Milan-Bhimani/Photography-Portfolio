9da924ac510c613bb7dbbe51a706de5d0ec5b55b
Milan Bhimani <milanhbhimani@gmail.com>
2025-01-11 10:37:48 +0530

    Update api.js

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const API = axios.create({ baseURL: 'https://photography-portfolio-kkqg.onrender.com' });
+const API = axios.create({ baseURL: 'https://photography-portfolio-six.vercel.app' });
 
 API.interceptors.request.use((req) => {
   const token = localStorage.getItem('token');
